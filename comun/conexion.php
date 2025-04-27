5c374f97470b7d1855c79709cad5c8d1853a8145
andrespaz19991 <andres.paz1991@gmail.com>
2025-04-27 13:27:19 -0500

    conexion

--- comun/conexion.php
+++ comun/conexion.php
@@ -1,29 +1,29 @@
 <?php
 error_reporting(E_ALL);
-$rutaPrincipal = $_SERVER['DOCUMENT_ROOT'].'/guagua/comun/';
-require_once ($rutaPrincipal."/funciones.php");
-require_once ($rutaPrincipal."/config.php");
-$mysqli = new mysqli (SERVIDORBD, USUARIOBD, CLAVEBD, BASEDEDATOS);
-if (mysqli_connect_errno()){
-echo "error".mysqli_connect_errno();
-}else{
- if($mysqli){
-	#echo "si";
- 	  mysqli_set_charset($mysqli,'utf8');
- }
+
+require_once __DIR__ . "/funciones.php";
+require_once __DIR__ . "/config.php";
+
+$mysqli = new mysqli(SERVIDORBD, USUARIOBD, CLAVEBD, BASEDEDATOS);
+
+if (mysqli_connect_errno()) {
+    echo "error " . mysqli_connect_errno();
+} else {
+    mysqli_set_charset($mysqli, 'utf8');
 }
 
- ini_set('date.timezone', TIME_ZONE);
- date_default_timezone_set(TIME_ZONE);
- $mysqli->query("SET time_zone =  '".TIME_ZONE_OFFSET."'");
-//datos institucion
+ini_set('date.timezone', TIME_ZONE);
+date_default_timezone_set(TIME_ZONE);
+$mysqli->query("SET time_zone = '".TIME_ZONE_OFFSET."'");
+
+// Datos institución
 $sql_ie = "SELECT * FROM `institucion_educativa` WHERE id_institucion_educativa ='1'"; 
-if($consulta_ie = $mysqli->query($sql_ie)){
-$row_ie=$consulta_ie->fetch_assoc();
-if (!defined('NOMBRE_INSTITUCION')) if(isset($row_ie['nombre_institucion'])) define ("NOMBRE_INSTITUCION",$row_ie['nombre_institucion']);
-if (!defined('LOGO_INSTITUCION')) if(isset($row_ie['logo_institucion'])) define ("LOGO_INSTITUCION",$row_ie['logo_institucion']);
-if (!defined('BANNER_INSTITUCION')) if(isset($row_ie['BANNER_INSTITUCION'])) define ("BANNER_INSTITUCION",$row_ie['BANNER_INSTITUCION']);
-
+if ($consulta_ie = $mysqli->query($sql_ie)) {
+    $row_ie = $consulta_ie->fetch_assoc();
+    if (!defined('NOMBRE_INSTITUCION') && isset($row_ie['nombre_institucion'])) define("NOMBRE_INSTITUCION", $row_ie['nombre_institucion']);
+    if (!defined('LOGO_INSTITUCION') && isset($row_ie['logo_institucion'])) define("LOGO_INSTITUCION", $row_ie['logo_institucion']);
+    if (!defined('BANNER_INSTITUCION') && isset($row_ie['BANNER_INSTITUCION'])) define("BANNER_INSTITUCION", $row_ie['BANNER_INSTITUCION']);
 }
-require("permisos.php");//contiene las restricciones de acceso a carpetas y archivos de acuerdo al rol
+
+require_once __DIR__ . "/permisos.php"; // también aquí
 ?>
