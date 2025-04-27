5ee6990405cf8ea575584c586132f894500af438
andrespaz19991 <andres.paz1991@gmail.com>
2025-04-27 15:01:26 -0500

    conexion10

--- clases/Clase_mysqli.Class.php
+++ clases/Clase_mysqli.Class.php
@@ -35,10 +35,8 @@ class Clase_mysqli extends Comun {
        switch (NOTA) {
         case "no":
             $conexion_mysqli = new mysqli("srv765.hstgr.io", "u417538463_root", "Handres2025..", "u417538463_guagua");
-            echo "i es una no <br>";
             break;
         case "si":
-            echo "i es una si";
         $conexion_mysqli = new mysqli("127.0.0.1", "root", "", "guagua", 7000);
             break;
     }
